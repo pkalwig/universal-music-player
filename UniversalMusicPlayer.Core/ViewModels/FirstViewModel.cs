e583b402b1f196a34753f4b0ea8c10a1c2910539
Piotr Kalwig <p.kalwig88@gmail.com>
2017-11-19 00:26:47 +0100

    Move to MvvmCross

---
+++ UniversalMusicPlayer.Core/ViewModels/FirstViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.Core.ViewModels;
+
+namespace MvvmCrossDocs.Core.ViewModels
+{
+    public class FirstViewModel : MvxViewModel
+    {
+        public FirstViewModel()
+        {
+            Hello = "Hello MvvmCross";
+        }
+
+        private string _hello;
+        public string Hello
+        {
+            get => _hello;
+            set => SetProperty(ref _hello, value);
+        }
+    }
+}
--- UniversalMusicPlayer.UWP/App.xaml.cs
+++ UniversalMusicPlayer.UWP/App.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using MvvmCross.Platform;
+using MvvmCrossDocs.WindowsUniversal;
+using System;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace UniversalMusicPlayer.UWP
@@ -59,18 +51,22 @@ namespace UniversalMusicPlayer.UWP
                 Window.Current.Content = rootFrame;
             }
 
-            if (e.PrelaunchActivated == false)
-            {
                 if (rootFrame.Content == null)
                 {
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    //rootFrame.Navigate(typeof(MainPage), e.Arguments);
+
+                    var setup = new Setup(rootFrame);
+                    setup.Initialize();
+
+                    var start = Mvx.Resolve<MvvmCross.Core.ViewModels.IMvxAppStart>();
+                    start.Start();
                 }
+
                 // Ensure the current window is active
                 Window.Current.Activate();
-            }
         }
 
         /// <summary>
