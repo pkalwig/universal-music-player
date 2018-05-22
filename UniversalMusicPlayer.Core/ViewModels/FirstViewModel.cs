20b05a3bc2fe0a182db688168e61cd6df4e09e4d
Piotr Kalwig <p.kalwig88@gmail.com>
2018-05-22 23:53:38 +0200

    Update View and ViewModel

--- UniversalMusicPlayer.Core/ViewModels/FirstViewModel.cs
+++ UniversalMusicPlayer.Core/ViewModels/FirstViewModel.cs
@@ -1,43 +1,55 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using MvvmCross.Core.ViewModels;
-using UniversalMusicPlayer.Core.Data;
+using UniversalMusicPlayer.Core.Data.POs;
 using UniversalMusicPlayer.Core.Services;
 
 namespace UniversalMusicPlayer.Core.ViewModels
 {
     public class FirstViewModel : MvxViewModel
 	{
-        private readonly IFileService _fileServce;
+        private readonly IAudioItemsProviderService _audioItemsProviderService;
+        private bool _isScanning;
 
-        public FirstViewModel(IFileService fileServce)
+        public FirstViewModel(IAudioItemsProviderService audioItemsProviderService)
         {
-            _fileServce = fileServce;
-            ScanCommand = new MvxAsyncCommand(ScanAction);
+            _audioItemsProviderService = audioItemsProviderService;
+
+            ScanCommand = new MvxAsyncCommand(ScanActionAsync);
+            MusicLibrary = new ObservableCollection<AudioItemPO>();
         }
 
-        private ICollection<PlaylistItem> _musicLibrary;
-        public ICollection<PlaylistItem> MusicLibrary
+        public ICollection<AudioItemPO> MusicLibrary { get; }
+
+        private async Task ScanActionAsync()
         {
-            get => _musicLibrary;
-            set => SetProperty(ref _musicLibrary, value);
+            IsScanning = true;
+            var sw = new Stopwatch();
+            sw.Start();
+            MusicLibrary.Clear();
+            foreach(var item in await _audioItemsProviderService.GetAudioItemsAsync())
+            {
+                MusicLibrary.Add(item);
+            }
+            sw.Stop();
+            Debug.WriteLine($"Total time spent scanning: {sw.Elapsed}");
+
+            IsScanning = false;
         }
 
+        #region Properties
+        public bool IsScanning
+        {
+            get => _isScanning;
+            set => SetProperty(ref _isScanning, value);
+        }
+        #endregion
+
+        #region Commands
         public ICommand ScanCommand { get; }
-        private async Task ScanAction()
-        {
-            await _fileServce.ScanLocalLibraryAsync();
-            MusicLibrary = new ObservableCollection<PlaylistItem>(
-				_fileServce.AudioFiles.Select(
-					audioFile => new PlaylistItem(audioFile, PlaySelectedAction)));
-        }
-
-		private void PlaySelectedAction(IAudioFile audioFile)
-		{
-			//TODO: some service that will play file
-		}
+        #endregion
     }
 }
