a07f197829a84ed380662c51da6d32bef9847b9b
Piotr Kalwig <p.kalwig88@gmail.com>
2018-05-22 23:20:05 +0200

    Move supported file extensions for scanner to config

---
+++ UniversalMusicPlayer.Core/Config/ConfigInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace UniversalMusicPlayer.Core.Config
+{
+    public static class ConfigInfo
+    {
+        public static IEnumerable<string> SupportedFileExtensions => new []
+        {
+            ".mp3",
+            ".m4a",
+            ".ogg",
+            ".flac"
+        };
+    }
+}
