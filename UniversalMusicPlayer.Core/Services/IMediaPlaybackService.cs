8d1da670bab28e36643e23bcec0600bb465eb209
Piotr Kalwig <p.kalwig88@gmail.com>
2018-12-15 21:54:49 +0100

    Add media playback PoC

---
+++ UniversalMusicPlayer.Core/Services/IMediaPlaybackService.cs
@@ -0,0 +1,10 @@
+﻿using UniversalMusicPlayer.Core.Data;
+
+namespace UniversalMusicPlayer.Core.Services
+{
+    public interface IMediaPlaybackService
+    {
+		IAudioFile SelectedAudioFile { get; set; }
+	    void Play<T>(IAudioFile<T> audioFile);
+    }
+}
---
+++ UniversalMusicPlayer.UWP/Services/Prod/MediaPlaybackService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.Media.Core;
+using Windows.Storage;
+using UniversalMusicPlayer.Core.Data;
+using UniversalMusicPlayer.Core.Services;
+
+namespace UniversalMusicPlayer.UWP.Services.Prod
+{
+	public class MediaPlaybackService : IMediaPlaybackService
+	{
+		public IAudioFile SelectedAudioFile { get; set; }
+
+		public void Play<T>(IAudioFile<T> audioFile)
+		{
+			var mediaSource = MediaSource.CreateFromStorageFile((audioFile as IAudioFile<StorageFile>)?.File);
+		}
+	}
+}
